3ef3e480bdb81875a9a2a020d4372793f439f220
GLi81 <90353988+gli81@users.noreply.github.com>
2023-10-29 05:22:50 +0000

    worked on lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -99,7 +99,8 @@ pub fn insert(
     embarked: &str,
 ) -> Result<(), rusqlite::Error> {
     c.execute(
-        "INSERT INTO titanic (survived,
+        "INSERT INTO titanic (
+            survived,
             p_class,
             name,
             sex,
@@ -110,7 +111,7 @@ pub fn insert(
             fare,
             cabin,
             embarked
-        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
+        ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)",
         params![survived, pclass, name, sex, age, sibsp, parch, ticket, fare, cabin, embarked],
     )?;
     Ok(())
@@ -119,24 +120,43 @@ pub fn insert(
 //write a function that reads the data from the database
 pub fn read(
     c: &rusqlite::Connection,
-) -> Result<Vec<(i64, String, String, f64, f64)>, rusqlite::Error> {
+) -> Result<(i32, i32, i32, String, String, String, i32, i32, String, f64, String, String), rusqlite::Error> {
     let mut stmt =
-        c.prepare("SELECT id, name_cap_2, num_rom_ca, Shape_Leng, Shape_Area FROM indexs")?;
-    let indexs_iter = stmt.query_map(NO_PARAMS, |row| {
+        c.prepare("SELECT
+        passenger_id,
+        survived,
+        p_class,
+        name,
+        sex,
+        age,
+        sib_sp,
+        parch,
+        ticket,
+        fare,
+        cabin,
+        embarked FROM titanic WHERE passenger_id = ?;")?;
+    let rslt = stmt.query_row([1], |row| {
         Ok((
             row.get(0)?,
             row.get(1)?,
             row.get(2)?,
             row.get(3)?,
             row.get(4)?,
+            row.get(5)?,
+            row.get(6)?,
+            row.get(7)?,
+            row.get(8)?,
+            row.get(9)?,
+            row.get(10)?,
+            row.get(11)?,
         ))
     })?;
 
-    let mut indexs = Vec::new();
-    for index in indexs_iter {
-        indexs.push(index?);
-    }
-    Ok(indexs)
+    // let mut indexs = Vec::new();
+    // for index in rslt {
+    //     indexs.push(index?);
+    // }
+    Ok(rslt)
 }
 
 //write a function that updates the shape length
